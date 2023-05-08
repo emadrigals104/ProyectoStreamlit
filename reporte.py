0c34e1764d88efd76f05c72c9f97f405c3cff992
Esteban Madrigal <44320667+emadrigals104@users.noreply.github.com>
2023-05-08 08:10:22 -0500

    Power BI Actualizado

--- reporte.py
+++ reporte.py
@@ -15,7 +15,7 @@ def reporte():
 
     ¡Disfruta explorando el reporte y descubriendo información útil para tu análisis!''')
     st.markdown('''
-                <iframe title="Transporte y Logistica - Instructor" width="1100" height="673.5" src="https://app.powerbi.com/view?r=eyJrIjoiZjVkODFhNjEtY2RhMy00ODAwLWExNGUtOWM0MTJkNjM1NDFkIiwidCI6ImQyZGZkYjc0LWZiNDItNGMzYi04ZGFkLTZiNzg1NzlmMmM4ZCIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+                <iframe title="Transporte y Logistica - Instructor" width="1100" height="673.5" src="https://app.powerbi.com/view?r=eyJrIjoiYzJkOWRiYzctMDE4NS00NTcyLTkwMGYtMDQ0NzkwMTZiZmVmIiwidCI6ImQyZGZkYjc0LWZiNDItNGMzYi04ZGFkLTZiNzg1NzlmMmM4ZCIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
                 ''', unsafe_allow_html=True)
 
 
